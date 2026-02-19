10f55b6b6b0dea847452846e0e1a4bb466365fcc
M Saad Mustafa <msaadmustafa091@gmail.com>
2026-02-19 10:09:59 -0800

    added deploy frontend url

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,8 @@ import path from 'path';
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin:"blog-app-znpr.vercel.app",
     credentials: true
 }));
 
